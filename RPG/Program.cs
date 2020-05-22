7d26b89413691382e350167a0883f648e2971385
Cameron Minchin <1022765@student.ccgs.wa.edu.au>
2020-05-22 09:12:18 +0800

    Initial commit

---
+++ RPG/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using System.Drawing;
+using System.Threading.Tasks;
+using Console = Colorful.Console;
+
+namespace RPG
+{
+    class Program
+    {
+        static void Options()
+        {
+            Print("There are no options.", "");
+            Console.Write("\r                     \r");
+            start();
+        }
+        static void quit()
+        {
+            Environment.Exit(1);
+        }
+        static void Print(string text, string end = "\n", int interval = 100)
+        {
+            foreach (char character in text)
+            {
+                Console.Write(character);
+                Thread.Sleep(interval);
+            }
+            Console.Write(end);
+        }
+
+        static void start()
+        {
+            Console.Write("\r");
+            string x = Console.ReadLine();
+            switch (x.ToLower())
+            {
+                case "1":
+                    break;
+                case "start":
+                    break;
+                case "2":
+                    Options();
+                    break;
+                case "options":
+                    Options();
+                    break;
+                case "3":
+                    quit();
+                    break;
+                case "exit":
+                    quit();
+                    break;
+                default:
+                    Print("Learn to spell\n");
+                    start();
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
